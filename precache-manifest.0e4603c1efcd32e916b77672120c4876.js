f5ccad6a7045c07068b17a4e53939137b8bbdd07
nathalia_ortega <natyan27@hotmail.com>
2019-08-28 17:03:13 -0500

    Updates

---
+++ precache-manifest.0e4603c1efcd32e916b77672120c4876.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dee32a0a900c31eb6089d18dd0b1222",
+    "url": "/Cocolatte/index.html"
+  },
+  {
+    "revision": "794facc8191c8646845b",
+    "url": "/Cocolatte/static/css/main.2450e757.chunk.css"
+  },
+  {
+    "revision": "91a212dda9a3e90c84de",
+    "url": "/Cocolatte/static/js/2.5e88ce3e.chunk.js"
+  },
+  {
+    "revision": "794facc8191c8646845b",
+    "url": "/Cocolatte/static/js/main.cac0ead7.chunk.js"
+  },
+  {
+    "revision": "ac364bed3bcff7943ccc",
+    "url": "/Cocolatte/static/js/runtime~main.ac6e9a51.js"
+  },
+  {
+    "revision": "01333697553f39c92c487ca739bb62d4",
+    "url": "/Cocolatte/static/media/DancingScript-Bold.01333697.woff2"
+  },
+  {
+    "revision": "3b48e577810f467aa65a50e037aef56d",
+    "url": "/Cocolatte/static/media/DancingScript-Bold.3b48e577.woff"
+  },
+  {
+    "revision": "24d6087e2cd251e9ad4f1a1924df0238",
+    "url": "/Cocolatte/static/media/DancingScript-Regular.24d6087e.woff2"
+  },
+  {
+    "revision": "5bce28875925bf58944c33ec9a6bc74a",
+    "url": "/Cocolatte/static/media/DancingScript-Regular.5bce2887.woff"
+  },
+  {
+    "revision": "807d7444b6c864467baaba48830b0508",
+    "url": "/Cocolatte/static/media/FondoBienvenidos.807d7444.png"
+  },
+  {
+    "revision": "6860fa24edbdd54a819776d0e488e71f",
+    "url": "/Cocolatte/static/media/FondoTitulo.6860fa24.png"
+  },
+  {
+    "revision": "e0d88558b69bd8382fa26f3d1f053607",
+    "url": "/Cocolatte/static/media/FondoTmp.e0d88558.JPG"
+  },
+  {
+    "revision": "759ea19bccf8a5ca50a58e474aced157",
+    "url": "/Cocolatte/static/media/OpenSans-Bold.759ea19b.woff"
+  },
+  {
+    "revision": "93fe8f3415688d16bb947e2c96ea380e",
+    "url": "/Cocolatte/static/media/OpenSans-Bold.93fe8f34.woff2"
+  },
+  {
+    "revision": "0a96d2322aa76287314064b8f9b55466",
+    "url": "/Cocolatte/static/media/OpenSans-BoldItalic.0a96d232.woff"
+  },
+  {
+    "revision": "183f1aa2eb319f83e073f83f72a94dbb",
+    "url": "/Cocolatte/static/media/OpenSans-BoldItalic.183f1aa2.woff2"
+  },
+  {
+    "revision": "1b2c3c4788d5f305789b49f7d38e85c5",
+    "url": "/Cocolatte/static/media/OpenSans-ExtraBold.1b2c3c47.woff2"
+  },
+  {
+    "revision": "3f760f718edb65cd95c6da14d6ab1bb8",
+    "url": "/Cocolatte/static/media/OpenSans-ExtraBold.3f760f71.woff"
+  },
+  {
+    "revision": "d69a748730ffaa1494fbd42d8b25233a",
+    "url": "/Cocolatte/static/media/OpenSans-ExtraBoldItalic.d69a7487.woff2"
+  },
+  {
+    "revision": "edbbf5335fcb1e037d0ed16c17364c16",
+    "url": "/Cocolatte/static/media/OpenSans-ExtraBoldItalic.edbbf533.woff"
+  },
+  {
+    "revision": "052b69c9c418c621fbd978d33e975b92",
+    "url": "/Cocolatte/static/media/OpenSans-Italic.052b69c9.woff"
+  },
+  {
+    "revision": "c27ebc192b3fe6167aa988bc77c97139",
+    "url": "/Cocolatte/static/media/OpenSans-Italic.c27ebc19.woff2"
+  },
+  {
+    "revision": "559e978f49dc58dbe4213c714d308483",
+    "url": "/Cocolatte/static/media/OpenSans-Light.559e978f.woff2"
+  },
+  {
+    "revision": "c002a58fc48427af4884fcce2d5c03ee",
+    "url": "/Cocolatte/static/media/OpenSans-Light.c002a58f.woff"
+  },
+  {
+    "revision": "d821368c1b1a2f95186faf4a99d30e89",
+    "url": "/Cocolatte/static/media/OpenSans-LightItalic.d821368c.woff2"
+  },
+  {
+    "revision": "f98d42be17e95311f1f82823fb3b6a77",
+    "url": "/Cocolatte/static/media/OpenSans-LightItalic.f98d42be.woff"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/Cocolatte/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/Cocolatte/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "7106bb83a0e767e12659de9cfba3926d",
+    "url": "/Cocolatte/static/media/OpenSans-SemiBold.7106bb83.woff2"
+  },
+  {
+    "revision": "fe241c6c4f5a190e821b7c03e337d756",
+    "url": "/Cocolatte/static/media/OpenSans-SemiBold.fe241c6c.woff"
+  },
+  {
+    "revision": "02669c88881b1577a54e9e2d76f25566",
+    "url": "/Cocolatte/static/media/OpenSans-SemiBoldItalic.02669c88.woff"
+  },
+  {
+    "revision": "da6f476d3784fdfb4b3325c491418c69",
+    "url": "/Cocolatte/static/media/OpenSans-SemiBoldItalic.da6f476d.woff2"
+  },
+  {
+    "revision": "2120841c8d6898af11c0f89bb4c5aa56",
+    "url": "/Cocolatte/static/media/coffee-cup.2120841c.png"
+  },
+  {
+    "revision": "e80b73c0fbd9116e1c63bff645265dcb",
+    "url": "/Cocolatte/static/media/coffeeGrain.e80b73c0.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/Cocolatte/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/Cocolatte/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/Cocolatte/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/Cocolatte/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/Cocolatte/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/Cocolatte/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/Cocolatte/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/Cocolatte/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/Cocolatte/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/Cocolatte/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/Cocolatte/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/Cocolatte/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/Cocolatte/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/Cocolatte/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/Cocolatte/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/Cocolatte/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cocolatte/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c7a04985e6698d82b925f4d61ae6191",
+    "url": "/Cocolatte/static/media/logoBlanco-xs.0c7a0498.png"
+  },
+  {
+    "revision": "fc601ba58f923366416bd69b8be6cca4",
+    "url": "/Cocolatte/static/media/wpp.fc601ba5.png"
+  }
+]);
